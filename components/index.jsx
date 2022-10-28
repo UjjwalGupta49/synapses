078a5727aae9fc780374287507fb1d6ea6495cfb
UjjwalGupta49 <ujjwalg056@gmail.com>
2022-10-28 22:23:37 +0530

    added bonfida domain resolver

--- components/index.jsx
+++ components/index.jsx
@@ -2,6 +2,7 @@ import { Navbar } from "./navbar"
 import { Banner } from "./banner"
 import { ProfilePic } from "./porfilePic" 
 
+
 const components = {Navbar, Banner, ProfilePic};
 
 export default components;
--- pages/[id].jsx
+++ pages/[id].jsx
@@ -2,20 +2,49 @@ import { useRouter } from "next/router";
 import { Spacer } from "@nextui-org/react";
 import styles from "../styles/static.module.css";
 import components from "../components";
+import { getDomainKey, NameRegistryState } from "@bonfida/spl-name-service";
+import { Connection, clusterApiUrl } from "@solana/web3.js";
+import { Button } from "@nextui-org/react";
+import { useEffect, useState } from "react";
 
 const profile = (props) => {
   const router = useRouter();
   const user = router.query.id;
+  const [owner, setOwner] = useState("");
+  const Domain = () => {
+    const domainName = `${user}` ; // With or without the .sol at the end
+  
+    const connection = new Connection(clusterApiUrl("mainnet-beta"), "confirmed");
+  
+    const getPubkey = async () => {
+      const { pubkey } = await getDomainKey(domainName);
+  
+      const { registry } = await NameRegistryState.retrieve(connection, pubkey);
+      setOwner(registry.owner) 
+  
+      console.log(`Owner: ${registry.owner}`);
+    };
+    useEffect(() => {
+      getPubkey();
+  }, [domainName]);
+
+  return(
+    <div>
+      <p></p>
+    </div>
+  )
+  
+  };
   return (
     <div className="min-h-screen m-0 " style={{ backgroundColor: "#CCE6FF" }}>
+      {Domain()}
       <components.Navbar />
       <Spacer />
-      <div style={{ display: "flex", justifyContent: "center", position: "absolute", width:"100%"}}>
+      <div style={{ display: "flex", justifyContent: "center", position: "absolute", width:"100%",  alignItems: "center"}}>
         <components.ProfilePic />
       </div>
       <components.Banner className="relative" />
 
-      {/* <p className="text-center font-semibold text-black text-2xl">gm! {user} 👋</p> */}
     </div>
   );
 };
@@ -24,3 +53,5 @@ export default profile;
 
 // resolve .sol name to pubkey, getting data for Wordcel articles, dilalect user name,
 // 1 tailwind spacing uint = 4px
+
+
